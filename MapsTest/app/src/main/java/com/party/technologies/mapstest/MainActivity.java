acf28ac7ce2590543083dba31dc418ea3dbde751
Vikram Sharma <vas360@nyu.edu>
2021-11-04 00:56:59 -0400

    Added maps testing project

---
+++ MapsTest/app/src/main/java/com/party/technologies/mapstest/MainActivity.java
@@ -0,0 +1,86 @@
+package com.party.technologies.mapstest;
+
+import static android.content.ContentValues.TAG;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * An activity that displays a Google map with a marker (pin) to indicate a particular location.
+ */
+// [START maps_marker_on_map_ready]
+public class MainActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
+
+    // [START_EXCLUDE]
+    // [START maps_marker_get_map_async]
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
+        setContentView(R.layout.activity_main);
+
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    // [END maps_marker_get_map_async]
+    // [END_EXCLUDE]
+
+    // [START_EXCLUDE silent]
+    /**
+     * Manipulates the map when it's available.
+     * The API invokes this callback when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user receives a prompt to install
+     * Play services inside the SupportMapFragment. The API invokes this method after the user has
+     * installed Google Play services and returned to the app.
+     */
+    // [END_EXCLUDE]
+    // [START maps_marker_on_map_ready_add_marker]
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // [START_EXCLUDE silent]
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        // [END_EXCLUDE]
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+        LatLng sydney = new LatLng(40.730610, -73.935242);
+        googleMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        // [START_EXCLUDE silent]
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // [END_EXCLUDE]
+    }
+    // [END maps_marker_on_map_ready_add_marker]
+}
+// [END maps_marker_on_map_ready]
