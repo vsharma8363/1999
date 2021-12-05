8328feab68ddcb5eae8a0d3fc4f9ad55f04a754a
Vikram Sharma <vsharma8363@protonmail.com>
2021-12-05 17:44:51 -0500

    Finished settings redirects

--- Android/app/src/main/java/com/party/technologies/nineteen_ninety_nine/ui/pages/Settings.java
+++ Android/app/src/main/java/com/party/technologies/nineteen_ninety_nine/ui/pages/Settings.java
@@ -11,11 +11,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.party.technologies.nineteen_ninety_nine.R;
 import com.party.technologies.nineteen_ninety_nine.data.user.User;
 import com.party.technologies.nineteen_ninety_nine.data.user.UserInterface;
 import com.party.technologies.nineteen_ninety_nine.ui.Home;
+import com.party.technologies.nineteen_ninety_nine.ui.LoginSignup;
 import com.party.technologies.nineteen_ninety_nine.ui.party_viewer.ViewParty;
 
 public class Settings extends AppCompatActivity {
@@ -67,49 +69,58 @@ public class Settings extends AppCompatActivity {
         safe_party_practices.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.bestcolleges.com/blog/college-party-safety/"));
+                startActivity(browserIntent);
             }
         });
         TextView feedback = findViewById(R.id.activity_settings_feedback);
         feedback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/vsharma8363/1999/issues"));
+                startActivity(browserIntent);
             }
         });
         TextView rate_our_app = findViewById(R.id.activity_settings_rate_our_app);
         rate_our_app.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/vsharma8363/1999/issues"));
+                startActivity(browserIntent);
             }
         });
         TextView privacy_policy = findViewById(R.id.activity_settings_privacy_policy);
         privacy_policy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/vsharma8363/1999/blob/main/Privacy%20Policy.md"));
+                startActivity(browserIntent);
             }
         });
         TextView alcohol_drug_hotline = findViewById(R.id.activity_settings_alcohol_hotline);
         alcohol_drug_hotline.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:18006624357"));
+                startActivity(intent);
             }
         });
         TextView opiate_resources = findViewById(R.id.activity_settings_opiate_resources);
         opiate_resources.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.hhs.gov/opioids/treatment/index.html"));
+                startActivity(browserIntent);
             }
         });
         TextView logout = findViewById(R.id.activity_settings_logout);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                UserInterface.logoutCurrentUser();
+                startActivity(new Intent(Settings.this, LoginSignup.class));
+                finish();
             }
         });
 
